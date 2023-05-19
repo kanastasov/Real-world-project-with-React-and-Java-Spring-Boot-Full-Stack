8e8435605c332deeb06e1fef37b0bda711bdcb20
Lubomir Anastasov <lubomir.anastasov@gmail.com>
2023-05-19 08:39:04 +0300

    Header with white color

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,7 +7,7 @@ import {LinkContainer} from 'react-router-bootstrap'
 
 const Header = () => {
     return (
-        <Navbar   className = 'color-change'>
+        <Navbar variant='dark' className = 'color-change'>
           <Container>
             <Navbar.Brand className = 'nav-dropdown' href="/">TheMovieDB</Navbar.Brand>
             <LinkContainer to ='/'> 
@@ -15,9 +15,9 @@ const Header = () => {
            </LinkContainer>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="me-auto">
+              <Nav className="me-auto" variant='dark'>
 
-                <NavDropdown  className = 'nav-dropdown' title="Movies" id="collasible-nav-dropdown">
+                <NavDropdown  className = 'nav-dropdown' title="Movies" id="collapsible-nav-dropdown1">
                   <NavDropdown.Item href="/">Popular</NavDropdown.Item>
                   <NavDropdown.Item href="/api/nowPlaying">                    Now Playing              </NavDropdown.Item>
                   <NavDropdown.Item href="/api/upcoming">Upcoming</NavDropdown.Item>                  
@@ -25,24 +25,24 @@ const Header = () => {
                 </NavDropdown>
 
 
-                <NavDropdown className = 'nav-dropdown' title="TV Shows" id="collasible-nav-dropdown">
+                <NavDropdown className = 'nav-dropdown' title="TV Shows" id="collapsible-nav-dropdown2">
                   <NavDropdown.Item href="/api/tv">Popular</NavDropdown.Item>
                   <NavDropdown.Item href="/api/tv/airingToday">Airing Today</NavDropdown.Item>
                   <NavDropdown.Item href="/api/tv/upcoming">On TV</NavDropdown.Item>                  
                   <NavDropdown.Item href="/api/tv/topRated">Top Rated</NavDropdown.Item>
                 </NavDropdown>
 
-                <NavDropdown className = 'nav-dropdown'  title="People" id="collasible-nav-dropdown">
+                <NavDropdown className = 'nav-dropdown'  title="People" id="collapsible-nav-dropdown3">
                   <NavDropdown.Item href="/api/people">Popular People</NavDropdown.Item>
                 </NavDropdown>
-                <NavDropdown className = 'nav-dropdown' title="More" id="collasible-nav-dropdown">
+                <NavDropdown className = 'nav-dropdown' title="More" id="collapsible-nav-dropdown4">
                   <NavDropdown.Item href="https://www.themoviedb.org/">Discussions</NavDropdown.Item>
                   <NavDropdown.Item href="https://www.themoviedb.org/leaderboard">Leaderboard</NavDropdown.Item>
                   <NavDropdown.Item href="https://www.themoviedb.org/talk">Support</NavDropdown.Item>                  
                   <NavDropdown.Item href="https://developer.themoviedb.org/docs">API</NavDropdown.Item>
                 </NavDropdown>  
               </Nav>
-              <Nav>
+              <Nav variant='dark'>
                 <Nav.Link className = 'nav-dropdown'  href="#deets">+</Nav.Link>
                 <Nav.Link className = 'nav-dropdown'  href="#deets">EN</Nav.Link>
 
@@ -60,4 +60,4 @@ const Header = () => {
       );
 }
  
-export default Header;<h1>Header</h1>
+export default Header;
