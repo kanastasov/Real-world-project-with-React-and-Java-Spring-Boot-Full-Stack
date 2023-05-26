af27ad00a59fca2537f0d1a8b54f24e6ab529dc4
Lubomir Anastasov <lubomir.anastasov@gmail.com>
2023-05-26 14:51:43 +0300

    tv-screen + current season

--- frontend/src/components/CurrentSeason.js
+++ frontend/src/components/CurrentSeason.js
@@ -8,38 +8,38 @@ const CurrentSeason = ({currentSesonEpisodes}) => {
     return (
         
            
-        <Card className='my-3 p-3 rounded'>
-       <a href={`/movie/${currentSesonEpisodes.id}`}>
-        <CardImg style={{height: '195px', width: '130px'}} src = {`https://image.tmdb.org/t/p/w500${currentSesonEpisodes.poster_path}`} variant = 'top'/>
-            </a>
-    <Card.Body>
-        <Col>
-            <Link to={`/currentSesonEpisodes/${currentSesonEpisodes?.id}`}>
-                <Card.Title style={{color: 'black'}} as ='div'>
+        <Card className='rounded current-season'>
+            <a href={`/movie/${currentSesonEpisodes.id}`}>
+                <CardImg style={{height: '195px', width: '130px'}} src = {`https://image.tmdb.org/t/p/w500${currentSesonEpisodes.poster_path}`} variant = 'top'/>
+            </a>        
+        <Card.Body>
+            <Col>
+                <Link to={`/currentSesonEpisodes/${currentSesonEpisodes?.id}`}>
+                    <Card.Title style={{color: 'black'}} as ='div'>
 
-                </Card.Title>
+                    </Card.Title>
+                
+                </Link>
             
-            </Link>
+            </Col>
+    
+            <Card.Text style={{color: 'black'}} as ='h2'>
+            <a href="#">{currentSesonEpisodes?.name}</a> 
+            </Card.Text>
         
-        </Col>
-   
-        <Card.Text style={{color: 'black'}} as ='h3'>
-        <p>{currentSesonEpisodes?.name}</p> 
-        </Card.Text>
-     
 
-        <Card.Text style={{color: 'black'}} as ='h3'>
-        <p>{currentSesonEpisodes?.air_date?.substring(0,4)} | {currentSesonEpisodes?.episodes?.length -1 }</p>
-        </Card.Text>
-     
+            <Card.Text style={{color: 'black'}} as ='h4'>
+            <p>{currentSesonEpisodes?.air_date?.substring(0,4)} | {currentSesonEpisodes?.episodes?.length -1 }</p>
+            </Card.Text>
+        
 
-        <Card.Text style={{color: 'black'}} as ='h4'>
-        <p>{currentSesonEpisodes?.overview}</p> 
-        </Card.Text>
+            <Card.Text style={{color: 'black'}} as ='div' className="season-overview">
+            <p>{currentSesonEpisodes?.overview}</p> 
+            </Card.Text>
 
-      
+        
 
-    </Card.Body>
+        </Card.Body>
         </Card>
         
         );
